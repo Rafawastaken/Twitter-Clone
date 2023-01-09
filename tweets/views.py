01d0bd7acdec44f278a9ff9a1e7899103ec9b3ef
Rafael Pimenta <jrafapimenta@gmail.com>
2023-01-09 23:21:04 +0000

    testing routes2

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Routes
+
+# Home view
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h2>Hello</h2>")
+
+# Tweet detail view
+def tweet_detail_view(request, tweet_id,  *args, **kwargs):
+    return HttpResponse(f"Tweet_detail_view: {tweet_id}")
