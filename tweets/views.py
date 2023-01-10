322d91e94d104a3035693ae44b6eede50899f12b
Rafael Pimenta <jrafapimenta@gmail.com>
2023-01-10 12:03:12 +0000

    tweet id

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-# Routes
+from .models import Tweet
 
 # Home view
 def home_view(request, *args, **kwargs):
@@ -10,4 +10,10 @@ def home_view(request, *args, **kwargs):
 
 # Tweet detail view
 def tweet_detail_view(request, tweet_id,  *args, **kwargs):
-    return HttpResponse(f"Tweet_detail_view: {tweet_id}")
+    try: # Try to find tweet by id
+        tweet = Tweet.objects.get(id = tweet_id)
+    except: # If tweet not found/doesnt exist
+        raise Http404 # Raise 404 not found
+    
+    return HttpResponse(f"Tweet id: {tweet.id}: {tweet.content}")
+
