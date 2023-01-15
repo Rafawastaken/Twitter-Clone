4b888b801a80fa973f471e8ea236c52f88b804b7
Rafael Pimenta <jrafapimenta@gmail.com>
2023-01-15 04:47:00 +0000

    user table setup using djanga default

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.conf import settings
 import random
 
+# User models
+User = settings.AUTH_USER_MODEL
+
+# Tweet class model
 class Tweet(models.Model):
     # id = models.AutoField(primary_key = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Associate users to tweets
     content = models.TextField(blank = True, null = True)
     image = models.FileField(upload_to = 'images/', blank = True, null = True)
 
